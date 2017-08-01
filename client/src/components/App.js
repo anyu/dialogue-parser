456647bfb34483056d9b33a6850919db29e59a64
An Yu <ayu3500@gmail.com>
2017-08-01 15:23:07 -0700

    (feat) Render input field, capture key input

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -4,12 +4,36 @@ import React from 'react';
 class App extends React.Component {
   constructor(props) {
     super(props);
+
+    this.state = {
+      inputURL: ''
+    }
+    this.captureInput = this.captureInput.bind(this);
+    this.submitURL = this.submitURL.bind(this);
+  }
+
+  captureInput(e) {
+    console.log(e.target.value)
+    this.setState({
+      inputURL: e.target.value
+    });
+  }
+
+  submitURL(url, e) {
+    e.preventDefault();
+    console.log('search submitted' + url); 
   }
 
   render() {
     return (
-      <div>
-        <h1>App is up!!</h1>
+      <div className="container">
+        <h1>Dialogue Parser</h1>
+        <div className="form_container">
+          <form onSubmit={(e)=> this.submitURL(this.state.inputURL, e)}>
+            <input type="text" placeholder="Enter URL" onChange={this.captureInput}/>
+            <button>Submit</button>
+          </form>
+        </div>
       </div>
   )}
 }
