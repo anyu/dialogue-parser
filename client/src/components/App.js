0657f24668c8b89d5975cd62f50b78b5d7e218db
An Yu <ayu3500@gmail.com>
2017-08-01 23:07:33 -0700

    (feat) Merge in parser code, retrieve results on URL submit

--- client/src/components/App.js
+++ client/src/components/App.js
@@ -1,19 +1,26 @@
 import React from 'react';
-
+const http = require('http');
+const xml2js = require('xml2js'),
+parser = new xml2js.Parser();
 
 class App extends React.Component {
   constructor(props) {
     super(props);
 
     this.state = {
-      inputURL: ''
+      inputURL: '',
+      numLines: ''
     }
     this.captureInput = this.captureInput.bind(this);
     this.submitURL = this.submitURL.bind(this);
+    this.processJSON = this.processJSON.bind(this);    
+    this.findKey = this.findKey.bind(this);
+    this.countLinesPerSpeech = this.countLinesPerSpeech.bind(this);
+    this.toTitleCase = this.toTitleCase.bind(this);
+    this.orderByCount = this.orderByCount.bind(this);
   }
 
   captureInput(e) {
-    console.log(e.target.value)
     this.setState({
       inputURL: e.target.value
     });
@@ -21,16 +28,96 @@ class App extends React.Component {
 
   submitURL(url, e) {
     e.preventDefault();
-    console.log('search submitted' + url); 
+    http.get(url, (res) => {
+      var data = '';
+      res.setEncoding('utf8');
+      res.on('data', (chunk) => {
+          data += chunk;
+      });
+      res.on('end', () => {
+        parser.parseString(data, (err, result) => {
+          if (err) {
+            console.log(err.message);
+          } else {
+            this.processJSON(result);
+          }
+        });
+      });
+      res.on('error', (err) => {
+        console.log(err.message);
+      });
+    });
+  }
+
+  findKey(jsonObj, searchTerm) {
+    var storage = [];
+
+    if (typeof(jsonObj) !== 'object') {
+      return null;
+    }
+    if (jsonObj.hasOwnProperty(searchTerm)) {
+      storage.push(jsonObj[searchTerm]);
+      return jsonObj[searchTerm];
+    } 
+    for (var key in jsonObj) {
+      storage = storage.concat(this.findKey(jsonObj[key], searchTerm));
+    }
+    return storage;
+  }
+
+  countLinesPerSpeech(speech) {
+    speech = speech.filter(Boolean);  // remove null values
+    var speechLines = {};
+
+    speech.map((dialogue) => {
+      var character = ''+ dialogue["SPEAKER"];
+      character = this.toTitleCase(character);
+      if (speechLines[character]) {
+        speechLines[character] += dialogue["LINE"].length;
+      } else {
+        speechLines[character] = dialogue["LINE"].length;
+      }
+    });
+    return speechLines;
+  }
+
+  toTitleCase(name) {
+    var formattedName = name.toLowerCase().split(' ')
+    .map(word => word[0].toUpperCase() + word.substr(1).toLowerCase())
+    .join(' ')
+    return formattedName;
+  }
+
+  // Sort number of lines, descending
+  orderByCount(obj) {
+    var sortable = [];
+    for (var character in obj) {
+      sortable.push([character, obj[character]]);
+    }
+    sortable.sort((a, b) => {
+      return b[1] - a[1];
+    });
+    return sortable;
+  }
+
+  processJSON(jsonData) {
+    var listOfSpeeches = this.findKey(jsonData, 'SPEECH');          
+    var linesPerSpeech = this.countLinesPerSpeech(listOfSpeeches);
+    var ordered = this.orderByCount(linesPerSpeech);
+    console.log(ordered)
+    this.setState({
+      numLines: ordered
+    })
   }
 
   render() {
+    console.log('ordered', this.state.numLines)
     return (
       <div className="container">
         <h1>Dialogue Parser</h1>
         <div className="form_container">
           <form onSubmit={(e)=> this.submitURL(this.state.inputURL, e)}>
-            <input type="text" placeholder="Enter URL" onChange={this.captureInput}/>
+            <input type="text" placeholder="http://" onChange={this.captureInput}/>
             <button>Submit</button>
           </form>
         </div>
