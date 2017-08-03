c64005baa03935423029536a7755e8aacb3de96c
An Yu <ayu3500@gmail.com>
2017-08-02 17:01:49 -0700

    (test) Add spec for convertXMLtoJSON function:

--- test/dialogue-parser.spec.js
+++ test/dialogue-parser.spec.js
@@ -1,23 +1,21 @@
 const chai = require('chai');
-const chaiHttp = require('chai-http');
+const sinon = require('sinon');
 const expect = require('chai').expect;
 
 const server = require('../server/server');
-const request = require('request');
 const dialogueParser = require("../script/dialogue-parser");
-
-chai.use(chaiHttp);
 
 
 describe('Dialogue Parser', function() {
 
-  // Tests convertXMLtoJSON function
-  // it('should convert XML datasource to JSON', function() {
-  //   var xmlSample = '<TITLE>The Tragedy of Macbeth</TITLE>'
-  //   var JSONtext = dialogueParser.convertXMLtoJSON(xmlSample);
-
-  //   expect(JSONtext).to.deep.equal('{ "TITLE": "The Tragedy of Macbeth" }')
-  // });
+  //Tests convertXMLtoJSON function
+  it('should convert XML datasource to JSON', function() {
+    var url = 'http://www.ibiblio.org/xml/examples/shakespeare/macbeth.xml';
+    const spy = sinon.spy();
+    sinon.stub(dialogueParser,'processJSON').callsFake(spy);
+    dialogueParser.convertXMLtoJSON(url);
+    expect(spy).called;
+  });
 
   // Tests findKey function
   it('should find all values of search term key in a nested JSON object', function() {
@@ -25,7 +23,7 @@ describe('Dialogue Parser', function() {
                  [{ "id": "5002", "type": "Glazed" },{ "id": "5005", "type": "Sugar" },
                      { "id": "5007", "type": "Powdered Sugar" },{ "id": "5006", "type": "Chocolate with Sprinkles" },
                      { "id": "5003", "type": "Chocolate" },{ "id": "5004", "type": "Maple" }
-                 ]};    
+                 ]};
     var justTypes = ['donut','Glazed','Sugar','Powdered Sugar','Chocolate with Sprinkles','Chocolate','Maple'];
     var searchTerm = 'type';
     var result = dialogueParser.findKey(jsonObj,searchTerm);
@@ -34,9 +32,9 @@ describe('Dialogue Parser', function() {
 
   // Tests countLinesPerSpeech function
   it('should sum up number of lines per character in each speech chunk', function() {
-    var arr = [ [ 
+    var arr = [ [
         { SPEAKER: ["Eric"], LINE: ["Fun in the sun."] },
-        { SPEAKER: ["Evangeline"], LINE: ["Totally"] }, 
+        { SPEAKER: ["Evangeline"], LINE: ["Totally"] },
         { SPEAKER: ["Emilie"], LINE: ["Cool story", "No really"] }
     ]];
     var result = dialogueParser.countLinesPerSpeech(arr);
@@ -53,20 +51,20 @@ describe('Dialogue Parser', function() {
     expect(multiWordTitleCased).to.equal("Mister Ravioli");
   });
 
-    // Tests orderByCount function 
+    // Tests orderByCount function
   it('should order contents of object by descending value order', function() {
     var unsorted = { 'Bean Friend': 2,
                     'Eric': 45,
                     'Alfalfa': 15,
                     'Evangeline': 89,
                     'Emilie': 20
-                };          
+                };
     var sorted = [ [ 'Evangeline', 89 ],
                 [ 'Eric', 45 ],
                 [ 'Emilie', 20 ],
                 [ 'Alfalfa', 15 ],
                 [ 'Bean Friend', 2 ]
-                ];                   
+                ];
     expect(dialogueParser.orderByCount(unsorted)).to.deep.equal(sorted);
   });
 });
